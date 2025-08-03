5a68c403a98aa5ba3feae6943e881b8f6ab5f16d
Sauravpant1 <sauravpant822@gmail.com>
2025-08-04 06:11:10 +0700

    adding html files

---
+++ first.js
@@ -0,0 +1,55 @@
+//let human = 
+// {
+//    name:'Saurav',
+//    age:17,
+//    class:12
+//    
+//};
+//console.log(human);
+//let color = ['red' , 'yellow' , 'green'];
+//console.log(color);
+//typeof(color);
+//console.log(typeof(color));
+//console.log(typeof(human));    
+
+
+// function sayHello(){
+//     console.log('Hello World');
+// }
+// sayHello(); 
+
+// function name(FirstName , LastName) {
+   
+//     console.log(FirstName + ' ' + LastName);
+// }
+// name('Saurav','Pant');
+     
+
+//Recipe to boil water
+
+// let ingrediants={
+//     water : 'Any amount you want',
+//     bhadakudi : 'Should be able to atleast contain water'
+// }
+// console.log(ingrediants)
+
+// let  Recipe ={
+//     first:'start the fire',
+//     second:'pour water in bhadakudi',
+//     third:'wait till water boils',
+//     fourth:'pour water in glass'
+// }
+// console.log(Recipe)
+
+
+// function sayHello() {
+//     alert('Hello World');
+// }
+
+// sayHello();
+
+
+
+// document.getElementById('p1').addEventListenereventlistener('click', function() {
+//     alert('Hello World');
+// });
